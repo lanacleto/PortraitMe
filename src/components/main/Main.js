55f591e7eaf11b5422ea12aeddcefebb688204ba
Diogo Paz <diogoachiles@gmail.com>
2022-10-20 15:37:49 -0300

    create file structure

---
+++ src/components/main/Main.js
@@ -0,0 +1,9 @@
+import "./Main.scss";
+
+function Main() {
+    return (
+        <div className="Main">Eu sou um componente main sendo renderizado</div>
+    );
+}
+
+export default Main;
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,7 @@
+import "./Sidebar.scss";
+
+function Sidebar() {
+    return <div className="Sidebar"></div>;
+}
+
+export default Sidebar;
